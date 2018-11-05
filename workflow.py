f60e20d7eea216af8c13ffd77db7aea615ed4fa4
andynet <andrejbalaz001@gmail.com>
2018-11-05 12:17:29 +0100

    added notes to workflow

---
+++ workflow.py
@@ -0,0 +1,12 @@
+from gwf import Workflow
+
+# prepare_input.py example vcf
+
+# ./shapeit --input-vcf ../../example/chimp_chr9_100.vcf
+#           --input-map ../example/genetic_map.txt
+#           --output-max ../example/gwas.phased.haps ../example/gwas.phased.sample
+
+# ./duohmm -H duohmm-example \
+#          -M genetic_map_chr10_combined_b37.txt \
+#          -O duohmm-example-corrected
+
